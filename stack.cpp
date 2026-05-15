233a64c8be0c311c92603c843301ece5cacf6861
sushiiyay <nfa.aliya7811@gmail.com>
2026-05-15 15:02:54 +0700

    Menambahkan switch

--- stack.cpp
+++ stack.cpp
@@ -90,5 +90,11 @@ int main ()
         cin >> choice;
 
         switch (choice)
+        case 1;
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value);
+            break;
+        case 2;
     }
 }
