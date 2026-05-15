b90b38299ed74e6675b50db3c6b7e651eb77a347
sushiiyay <nfa.aliya7811@gmail.com>
2026-05-15 14:40:23 +0700

    Stack is empty

--- stack.cpp
+++ stack.cpp
@@ -37,9 +37,15 @@ public:
 
     void pop()
     {
+        Node *temp = top;
         if (isEmpty())
         {
             cout << "Stack is empty." << endl;
+            return;
         }
+
+        cout << "Popped value: " << temp->data << endl;
+        top = temp->next;
+        delete temp;
     }
 }
