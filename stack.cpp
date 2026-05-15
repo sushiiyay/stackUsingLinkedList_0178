9474bca76ff5a05d5b9b15f6410be34f435c625c
sushiiyay <nfa.aliya7811@gmail.com>
2026-05-15 14:53:12 +0700

    Menambahkan bool is empty

--- stack.cpp
+++ stack.cpp
@@ -55,5 +55,17 @@ public:
         {
             cout << "List is empty." << endl;
         }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
     }
+
+    bool isEmpty()
 }
