91fe4806f6ff48b1816813e7560e9dcfd8d72b6e
sushiiyay <nfa.aliya7811@gmail.com>
2026-05-15 14:58:40 +0700

    Menambahkan cout di int main

--- stack.cpp
+++ stack.cpp
@@ -81,4 +81,11 @@ int main ()
     int value;
 
     while (choice != 5)
+    {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+    }
 }
